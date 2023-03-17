55ba016e85c9e86d54bc458ec1146178e6996b3c
Gustavo de Matos <gfmatos@Gustavos-MacBook-Air.local>
2023-03-16 22:05:46 -0300

    refeito pelo chat

--- clause_simplifier.py
+++ clause_simplifier.py
@@ -1,26 +1,20 @@
 import openai
 import streamlit as st
 
+# Set OpenAI API key from Streamlit secrets
 openai.api_key = st.secrets["key_openai"]
 
-st.title("Hi LEX!")
-st.spinner(text="Simplifying...")
+# Define Streamlit app title and sidebar content
+st.set_page_config(page_title="Clause Craft")
+st.sidebar.markdown("# Configs")
+output_lang = st.sidebar.radio("Output Language:", ["English", "Spanish", "Portuguese"])
+st.sidebar.markdown("# About")
+st.sidebar.markdown("Clause Craft is a mini-app that uses GPT-3 to rewrite clauses in plain language.")
+st.sidebar.markdown("Made by [Gustavo Matos](https://www.linkedin.com/in/gfmatos/)")
 
-
-with st.sidebar:
-    st.markdown("""
-    # About 
-    This is a mini-app that uses GPT-3 to rewrite clauses.  
-    """)
-    st.markdown("""
-    Made by [Gustavo Matos](https://www.linkedin.com/in/gfmatos/)
-    """)
-
-
-
-
+# Define function to generate a response using OpenAI GPT-3 API
 def generate_response(prompt):
-    completions = openai.Completion.create (
+    completions = openai.Completion.create(
         engine="text-davinci-003",
         prompt=prompt,
         max_tokens=1624,
@@ -28,18 +22,22 @@ def generate_response(prompt):
         stop=None,
         temperature=0.7,
     )
-
     message = completions.choices[0].text
     return message
 
-
-
+# Define function to get user input from a text area
 def get_input():
-    input_text = st.text_area("Paste your clause", label_visibility="hidden", placeholder="Paste your clause", height=300)
+    input_text = st.text_area("Paste your clause", height=300)
     return input_text
 
-user_input = get_input()
+# Define Streamlit app content
+def main():
+    st.title("Clause Craft")
+    user_input = get_input()
+    if st.button("Craft!"):
+        prompt = f"Rewrite the clause in {output_lang} {st.secrets['prompt_hilex2']} {user_input}"
+        response = generate_response(prompt)
+        st.write(response)
 
-if st.button("Simplify"):
-    prompt =" ".join([st.secrets["prompt_hilex2"], user_input])
-    st.write(generate_response(prompt))
+if __name__ == "__main__":
+    main()
