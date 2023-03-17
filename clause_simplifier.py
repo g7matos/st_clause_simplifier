3b5cbba22912de77e0a666961f3f228976da8208
Gustavo de Matos <gfmatos@Gustavos-MacBook-Air.local>
2023-03-16 21:34:54 -0300

    done

--- clause_simplifier.py
+++ clause_simplifier.py
@@ -4,20 +4,29 @@ import streamlit as st
 openai.api_key = st.secrets["key_openai"]
 
 st.title("Hi LEX!")
-st.markdown(
-    "This mini-app that simpyfies contractual clauses using OpenAI's GPT-3 based [Davinci model](https://beta.openai.com/docs/models/overview)"
-)
 st.spinner(text="Simplifying...")
+
+
+with st.sidebar:
+    st.markdown("""
+    # About 
+    This is a mini-app that uses GPT-3 to rewrite clauses.  
+    """)
+    st.markdown("""
+    Made by [Gustavo Matos](https://www.linkedin.com/in/gfmatos/)
+    """)
+
+
 
 
 def generate_response(prompt):
     completions = openai.Completion.create (
         engine="text-davinci-003",
         prompt=prompt,
-        max_tokens=1024,
+        max_tokens=1624,
         n=1,
         stop=None,
-        temperature=0.3,
+        temperature=0.7,
     )
 
     message = completions.choices[0].text
@@ -26,14 +35,11 @@ def generate_response(prompt):
 
 
 def get_input():
-	input_text = st.text_area("Paste your clause", label_visibility="hidden", placeholder="Paste your clause", height=300)
-	return input_text
-
+    input_text = st.text_area("Paste your clause", label_visibility="hidden", placeholder="Paste your clause", height=300)
+    return input_text
 
 user_input = get_input()
 
-st.write(st.secrets["prompt_hilex"] + user_input)
-
-if user_input:
-	output = generate_response(st.secrets["prompt_hilex"] + user_input)
-	st.write(output)
+if st.button("Simplify"):
+    prompt =" ".join([st.secrets["prompt_hilex2"], user_input])
+    st.write(generate_response(prompt))
